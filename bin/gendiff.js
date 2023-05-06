96a007c9fd5c086921e9375e2f309b2ed2a56556
Lo0lode <arthassas@mail.ru>
2023-05-06 14:46:30 +0300

    work?

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,13 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
+  .version('0.8.0')
   .name('string-util')
   .description('CLI to some JavaScript string utilities')
-  .version('0.8.0');
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((pathOne, pathTwo) => {
+    const diff = genDiff(pathOne, pathTwo, program.opts().format);
+    console.log(diff);
+  })
 program.parse(process.argv);
