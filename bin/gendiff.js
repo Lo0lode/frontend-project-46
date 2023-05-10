56a579e7ba4d2e4025e13eabad2a5b7ea0abf6ac
Max <arthassas@mail.ru>
2023-05-10 11:46:38 +0300

    work

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,14 @@
-import { Command } from 'commander';
+import {Command} from "commander";
+import genDiff from '../src/index.js'
 
 const program = new Command();
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format <type>', 'output format')
     .version('0.0.1', '-V, --version', 'output the version number')
-    .parse(process.argv)
+    .option('-f, --format <type>', 'output format')
+    .action((pathOne, pathTwo) => {
+        const diff = genDiff(pathOne, pathTwo, program.opts().format);
+        console.log(diff);
+      });
+    program.parse(process.argv)
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from 'lodash'
+import fs from 'fs'
+import { resolve } from 'path'
+
+export default function showDiff(filepath1,filepath2){
+    const readFile1 = fs.readFileSync(filepath1)
+    const readFile2 = fs.readFileSync(filepath2)
+    console.log('dhauisgdyas')
+}
