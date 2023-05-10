aeb4b4519f4146aff6e11e5978987894612c6430
Max <arthassas@mail.ru>
2023-05-10 09:46:43 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,14 @@
 const { Command } = require('commander');
+import genDiff from '../index.js';
 const program = new Command();
 
 program
-  .version('0.8.0')
-  .name('string-util')
-  .description('CLI to some JavaScript string utilities')
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((pathOne, pathTwo) => {
     const diff = genDiff(pathOne, pathTwo, program.opts().format);
     console.log(diff);
-  })
+  });
 program.parse(process.argv);
