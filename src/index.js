971dc67374959ba661754c3751c7928341b097e4
Max <arthassas@mail.ru>
2023-05-10 12:53:24 +0300

    work commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import _ from 'lodash'
 import fs from 'fs'
 import path from 'path'
 
-/* const getFormat = (file) => path.extname(file).replace(/\./, ''); */
 
 export default function showDiff(filepath1,filepath2){
-    //  const rightPathFile1 = getFormat(filepath1)
-     const readFile1 = fs.readFileSync(filepath1)
+    const readFile1 = fs.readFileSync(filepath1)
     const readFile2 = fs.readFileSync(filepath2)
-    console.log('ss   ' +readFile2 + '    ss')
+    console.log('ss   ' +readFile1 + '    ss')
 }
